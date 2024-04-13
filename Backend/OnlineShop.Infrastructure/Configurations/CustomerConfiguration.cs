b2e1b5e0f653e2366a5aed1e6c70f8983d02d2c0
Aulrata <Aulrata@gamil.com>
2024-04-13 18:43:15 +0300

    feat: customer configuration

---
+++ Backend/OnlineShop.Infrastructure/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineShop.Infrastructure.Entities;
+
+namespace OnlineShop.Infrastructure.Configurations;
+
+public class CustomerConfiguration : IEntityTypeConfiguration<CustomerEntity>
+{
+    public void Configure(EntityTypeBuilder<CustomerEntity> builder)
+    {
+        builder.HasKey(c => c.Id);
+    }
+}
