bdc747226b798d132d3b9f3cf1ab3159b92141d7
Aulrata <Aulrata@gamil.com>
2024-04-13 18:11:57 +0300

    feat: IRepository added

---
+++ Backend/OnlineShop.Domain/Abstractions/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineShop.Domain.Abstractions;
+
+public interface IRepository<T>
+{
+    Task Create(T entity);
+
+    Task<List<T>> Get();
+
+    Task<T> Update(T entity);
+
+    Task Delete(T entity);
+}
