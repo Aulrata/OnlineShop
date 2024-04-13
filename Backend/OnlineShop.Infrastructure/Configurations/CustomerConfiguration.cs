43a89be8b91643e00ff4474bc71d1985bcc435bb
Aulrata <Aulrata@gamil.com>
2024-04-14 01:05:05 +0300

    chore: CustomerConfiguration.cs changed

--- Backend/OnlineShop.Infrastructure/Configurations/CustomerConfiguration.cs
+++ Backend/OnlineShop.Infrastructure/Configurations/CustomerConfiguration.cs
@@ -9,5 +9,12 @@ public class CustomerConfiguration : IEntityTypeConfiguration<CustomerEntity>
     public void Configure(EntityTypeBuilder<CustomerEntity> builder)
     {
         builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.Name)
+            .IsRequired();
+        
+        builder.Property(c => c.Email)
+            .IsRequired();
+        
     }
 }
