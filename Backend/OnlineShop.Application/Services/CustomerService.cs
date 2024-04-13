c5fcc4bdb768744ab5b96f0db162d47fdf86aec3
Aulrata <Aulrata@gamil.com>
2024-04-14 01:56:01 +0300

    feat: methods create and get added to CustomerService

---
+++ Backend/OnlineShop.Application/Services/CustomerService.cs
@@ -0,0 +1,35 @@
+﻿using OnlineShop.Domain.Abstractions;
+using OnlineShop.Domain.Contracts;
+using OnlineShop.Domain.Models.Customer;
+
+namespace OnlineShop.Application.Services;
+
+public class CustomerService : ICustomerService
+{
+    private readonly ICustomerRepository _customerRepository;
+
+    public CustomerService(ICustomerRepository customerRepository)
+    {
+        _customerRepository = customerRepository;
+    }
+    
+    public async Task CreateCustomer(CustomerModel model)
+    {
+        await _customerRepository.Create(model);
+    }
+
+    public async Task<List<CustomerModel>> GetAllCustomers()
+    {
+        return await _customerRepository.Get();
+    }
+
+    public Task<CustomerModel> UpdateCustomer(CustomerModel model)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteCustomer(CustomerModel model)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Backend/OnlineShop.Domain/Contracts/ICustomerService.cs
@@ -0,0 +1,14 @@
+﻿using OnlineShop.Domain.Models.Customer;
+
+namespace OnlineShop.Domain.Contracts;
+
+public interface ICustomerService
+{
+    Task CreateCustomer(CustomerModel model);
+
+    Task<List<CustomerModel>> GetAllCustomers();
+
+    Task<CustomerModel> UpdateCustomer(CustomerModel model);
+
+    Task DeleteCustomer(CustomerModel model);
+}
